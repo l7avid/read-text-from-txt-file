33fb809a452c62b8d7c5cb77e8827fce6e12a340
David Reina <davidereina@hotmail.com>
2023-10-09 14:56:58 -0500

    This is an example of how to read a text from a .txt file

---
+++ ReadFromTextFile/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ReadFromTextFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\david.reina\Documents\Personal Devs\Ruta N Udemy - C#\19 - Read From Text File\Readme.txt");
+
+            Console.WriteLine($"The file contains the next text:\n\n{text}");
+        }
+    }
+}
